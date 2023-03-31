57ce150cfdcd52187e96cfa7f261a4f5c453de90
Stephano Telolahy <>
2023-03-31 21:54:58 +0200

    add reducer

---
+++ RockPaperScissors/ChooseWeaponAction.swift
@@ -0,0 +1,11 @@
+//
+//  ChooseWeaponAction.swift
+//  RockPaperScissors
+//
+//  Created by Hugues Telolahy on 31/03/2023.
+//
+import ReSwift
+
+struct ChooseWeaponAction: Action {
+    var weapon: Weapon
+}
