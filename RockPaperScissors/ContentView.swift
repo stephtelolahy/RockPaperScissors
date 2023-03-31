b141a2d7c63b60280e6531629686a517b171eb28
Stephano Telolahy <>
2023-03-31 22:24:30 +0200

    design ui

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            Text("Message")
+            Spacer()
+            HStack {
+                actionView(player: "player1", weapon: "choosen")
+                actionView(player: "player2", weapon: "choosen")
+            }
+            Spacer()
+            HStack {
+                Button("Rock") {
+                    print("Rock")
+                }
+                Button("Paper") {
+                    print("Paper")
+                }
+                Button("Scissors") {
+                    print("Scissors")
+                }
+            }
+            Spacer()
         }
         .padding()
+    }
+    
+    private func actionView(player: String, weapon: String) -> some View {
+        VStack {
+            Text(player)
+            Text(weapon)
+        }
+
     }
 }
 
